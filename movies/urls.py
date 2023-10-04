bbee5444ff2833ce054f27b48f19f6d67527eb24
Gabriel Silva <gabrielsilvacarvalho@usp.br>
2023-10-04 16:58:39 -0300

    Criar aplicação CRUD de filmes

--- movies/urls.py
+++ movies/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = "movies"
 urlpatterns = [
-    path("", views.list_movies, name="index"),
+    path('', views.MovieListView.as_view(), name='index'),
     path("search/", views.search_movies, name="search"),
     path("create/", views.create_movie, name="create"),
-    path("<int:movie_id>/", views.detail_movie, name="detail"),
+    path('<int:pk>/', views.MovieDetailView.as_view(), name='detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,10 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 from .temp_data import movie_data
-
+from .models import Movie
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .forms import MovieForm
 
 def detail_movie(request, movie_id):
     context = {"movie": movie_data[movie_id - 1]}
@@ -39,3 +42,43 @@ def create_movie(request):
         return HttpResponseRedirect(reverse("movies:detail", args=(len(movie_data),)))
     else:
         return render(request, "movies/create.html", {})
+    
+class MovieListView(generic.ListView):
+    model = Movie
+    template_name = 'movies/index.html'
+
+class MovieDetailView(generic.DetailView):
+    model = Movie
+    template_name = 'movies/detail.html'
+
+def create_movie(request):
+    if request.method == 'POST':
+        movie_name = request.POST['name']
+        movie_release_year = request.POST['release_year']
+        movie_poster_url = request.POST['poster_url']
+        movie = Movie(name=movie_name,
+                      release_year=movie_release_year,
+                      poster_url=movie_poster_url)
+        movie.save()
+        return HttpResponseRedirect(reverse('movies:detail',
+                                            args=(movie.id, )))
+    else:
+        return render(request, 'movies/create.html', {})
+    
+def create_movie(request):
+    if request.method == 'POST':
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            movie_name = form.cleaned_data['name']
+            movie_release_year = form.cleaned_data['release_year']
+            movie_poster_url = form.cleaned_data['poster_url']
+            movie = Movie(name=movie_name,
+                          release_year=movie_release_year,
+                          poster_url=movie_poster_url)
+            movie.save()
+            return HttpResponseRedirect(
+                reverse('movies:detail', args=(movie.id, )))
+    else:
+        form = MovieForm()
+    context = {'form': form}
+    return render(request, 'movies/create.html', context)
